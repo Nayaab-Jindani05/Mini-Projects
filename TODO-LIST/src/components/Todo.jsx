8da33e57ee8d8fc08a19a425e880235566778d96
Nayaab-Jindani05 <d2022.nayaab.jindani@ves.ac.in>
2025-08-31 12:50:45 +0530

    Update Todo.jsx

--- TODO-LIST/src/components/Todo.jsx
+++ TODO-LIST/src/components/Todo.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const Todo = ({ todo,deleteTodo,moveUp,moveDown,total,index}) => {
-    
-    return (
-        <div className='todo'>
-            <p className='text'>{todo.text}</p>
-                <div className='utils'>
-                <div className='go-up' style={{borderBottomColor:index === 0 ? 'grey' : '#1DA1F2'}} onClick={() => moveUp(todo.id)}></div>
-                <div className='go-down' style={{borderTopColor:index === total - 1 ? 'grey' : '#1DA1F2'}} onClick={() => moveDown(todo.id)}></div>
-                <div className='remove'  onClick={() => deleteTodo(todo.id)}></div>
-            </div>
-        </div>
-    )
-}
+const Todo = ({ todo, deleteTodo, moveUp, moveDown, total, index }) => {
+  const formattedDate = new Date(todo.date).toLocaleDateString(); // Format date nicely
 
-export default Todo
+  return (
+    <div className='todo'>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <p className='text'>{todo.text}</p>
+        <span style={{ fontSize: '0.8em', color: 'grey' }}>{formattedDate}</span>
+      </div>
+      <div className='utils'>
+        <div
+          className='go-up'
+          style={{ borderBottomColor: index === 0 ? 'grey' : '#1DA1F2' }}
+          onClick={() => moveUp(todo.id)}
+        ></div>
+        <div
+          className='go-down'
+          style={{ borderTopColor: index === total - 1 ? 'grey' : '#1DA1F2' }}
+          onClick={() => moveDown(todo.id)}
+        ></div>
+        <div className='remove' onClick={() => deleteTodo(todo.id)}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
